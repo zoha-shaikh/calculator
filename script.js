a5b6e5af2c28b933608e29c99716485446abf457
zoha-shaikh <zzzohashaikh7@gmail.com>
2025-08-29 19:41:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let display=document.querySelector("#display")
+function sinf(){
+    display.value=Math.sin(display.value)
+}
+function cosf(){
+    display.value=Math.cos(display.value)
+}
+function tanf(){
+    display.value=Math.tan(display.value)
+}
+function sqrf(){
+    display.value=Math.sqrt(display.value)
+}
